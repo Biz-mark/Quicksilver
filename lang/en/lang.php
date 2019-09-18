ea4925ac782cb933fcc0f30d7e029fce80785940
Alex <44265145+PubliAlex@users.noreply.github.com>
2019-09-18 11:01:34 +0200

    Update lang.php
    Add new localization strings related to the new dashboard widget

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -4,5 +4,12 @@ return [
     'plugin' => [
         'name' => 'Quicksilver',
         'description' => 'Lightning fast cache for your static website',
-    ]
+    ],
+    'reportwidgets' => [
+        'clearcache' => [
+            'name' => 'Clear Cache',
+            'success' => 'Cache cleared',
+            'button' => 'Clear cache',
+        ],
+    ],
 ];
