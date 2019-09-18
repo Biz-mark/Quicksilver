9c07e23bbe3e8258918a27168ad1e46c03a49f3b
Alex <44265145+PubliAlex@users.noreply.github.com>
2019-09-18 11:02:16 +0200

    Update lang.php
    Add new localization string related to the new report widget (to be translated cause I don't speak Russian at all ^^)

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -4,5 +4,12 @@ return [
     'plugin' => [
         'name' => 'Quicksilver',
         'description' => 'Супербыстрый кеш для вашего статичного сайта.',
-    ]
+    ],
+    'reportwidgets' => [
+        'clearcache' => [
+            'name' => 'Clear Cache',
+            'success' => 'Cache cleared',
+            'button' => 'Clear cache',
+        ],
+    ],
 ];
